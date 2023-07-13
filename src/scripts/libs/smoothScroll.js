4e1138cdbda88d6919f0463947ff08ec7232c41a
exslym <76844562+exslym@users.noreply.github.com>
2023-07-13 10:39:08 +0400

    modules & scripts updated, new added

--- src/scripts/libs/smoothScroll.js
+++ src/scripts/libs/smoothScroll.js
@@ -1,42 +1,46 @@
 //* Smooth navigation scroll
 export function smoothScroll(anchorClass) {
-	if (document.querySelector(`.${anchorClass}`)) {
-		document.querySelectorAll(`.${anchorClass}`).forEach(link => {
-			link.addEventListener('click', function (e) {
-				//* for tags <a></a>
-				if (link.getAttribute('href')) {
-					if (document.getElementById(link.getAttribute('href').substring(1))) {
-						e.preventDefault();
-						const href = link.getAttribute('href').substring(1);
-						const scrollTarget = document.getElementById(href);
-						scrollToTaarget(scrollTarget);
-					}
-				} else if (link.getAttribute('data-target')) {
-					//* for tags <button></button>
-					if (document.getElementById(link.dataset.target)) {
-						e.preventDefault();
-						const targetElement = link.dataset.target;
-						const scrollTarget = document.getElementById(targetElement);
-						scrollToTaarget(scrollTarget);
-					}
-				}
-			});
-		});
-	}
+  if (document.querySelector(`.${anchorClass}`)) {
+    document.querySelectorAll(`.${anchorClass}`).forEach((link) => {
+      link.addEventListener('click', (e) => {
+        switch (true) {
+          //* for tags e.g. <a href="#block"></a>
+          case link.hasAttribute('href'):
+            if (document.getElementById(link.getAttribute('href').substring(1))) {
+              e.preventDefault();
+              const href = link.getAttribute('href').substring(1);
+              const scrollTarget = document.getElementById(href);
+              scrollToTaarget(scrollTarget);
+            }
+            break;
+
+          //* for tags e.g. <button data-target="element"></button>
+          case link.hasAttribute('data-target'):
+            if (document.getElementById(link.dataset.target)) {
+              e.preventDefault();
+              const targetElement = link.dataset.target;
+              const scrollTarget = document.getElementById(targetElement);
+              scrollToTaarget(scrollTarget);
+            }
+            break;
+        }
+      });
+    });
+  }
 }
 
 function scrollToTaarget(scrollTarget) {
-	//! если не нужен отступ сверху:
-	// const topOffset = 0;
-	//! если есть fixed:
-	// document.querySelector('.scrollto').offsetHeight;
+  //! если не нужен отступ сверху:
+  // const topOffset = 0;
+  //! если есть fixed:
+  // document.querySelector('.scrollto').offsetHeight;
 
-	const topOffset = 0;
-	const elementPosition = scrollTarget.getBoundingClientRect().top;
-	const offsetPosition = elementPosition - topOffset;
+  const topOffset = 0;
+  const elementPosition = scrollTarget.getBoundingClientRect().top;
+  const offsetPosition = elementPosition - topOffset;
 
-	window.scrollBy({
-		top: offsetPosition,
-		behavior: 'smooth',
-	});
+  window.scrollBy({
+    top: offsetPosition,
+    behavior: 'smooth',
+  });
 }
